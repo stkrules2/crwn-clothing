ba483cf93b54a54a19cbda080aa1019c4a2b3881
stkrules2 <sulmanazhar10@gmail.com>
2020-10-16 01:21:59 +0500

    lesson 6 complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header-component/header.component.jsx";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header-component/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/shop" component={ShopPage} />
+				<Route exact path="/signin" component={SignInAndSignUp} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
