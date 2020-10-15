c555b64b26d62cf284c2f0048c157cf073ba8a15
stkrules2 <sulmanazhar10@gmail.com>
2020-10-15 19:38:30 +0500

    added header component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import './homepage.style.scss';
-import Directory from '../../components/directory/directory.component';
-const HomePage = (props) => {
-console.log(props);
-    return (
-        <div className='homepage'>
-            <Directory />
-        </div>
-    );
-}
+import React from "react";
+import "./homepage.style.scss";
+import Directory from "../../components/directory/directory.component";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
 
 export default HomePage;
