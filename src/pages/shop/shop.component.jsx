e2b386ed833a8340393f801a6e3a84818402404b
stkrules2 <sulmanazhar10@gmail.com>
2020-10-29 01:27:13 +0500

    redux 2 and storage and now was doing advanced routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,16 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...othercollectionprops }) => (
-          <CollectionPreview key={id} {...othercollectionprops} />
-        ))}
-      </div>
-    );
-  }
-}
+import CollectionsOverview from '../../components/collections-overview/collections-overview.componenet';
+import CategoryPage from '../category/category.component';
+
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+  </div>
+);
+
 
 export default ShopPage;
