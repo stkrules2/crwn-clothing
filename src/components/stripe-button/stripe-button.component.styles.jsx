afc35754e8349bef93704436ec88ee9e5eeef3e0
stkrules2 <sulmanazhar10@gmail.com>
2020-10-30 04:15:47 +0500

    heroku deployment

---
+++ src/components/stripe-button/stripe-button.component.styles.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HcnbUAdnEaiVC4zASngqGALTEPEcEREitM6QviMahNbpwlwTxj4fSust3LtJ87AxV46b0wZg4qWQ20BP8ZkeIlY0046kspjw4';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is ${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
