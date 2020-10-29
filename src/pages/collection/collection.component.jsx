fa37f7b00777286157220e68ac8752bbcebe895a
stkrules2 <sulmanazhar10@gmail.com>
2020-10-30 01:18:18 +0500

    Changes shop data to objects like in firebase

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,10 +8,15 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 const Collection = ({ collection }) => {
-    console.log(collection);
+    const { title, items } = collection;
     return (
-        <div className='collection'>
-            <h2>CATEGORY PAGE</h2>
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
         </div>
     )
 }
